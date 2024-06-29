7391c9282bb8463ba1534c233d4f2e2cfdd09fe0
Sudenaz Mert <sudenazmert5@gmail.com>
2024-06-29 16:48:55 +0300

    File updated

--- eucledianDistance.py
+++ eucledianDistance.py
@@ -1,1 +1,19 @@
-print ("Hello World!")
+import math
+
+#Points
+points = [(6, 1), (4, 6), (-1, 3), (11, 8), (3, 9)]
+
+#Function for eucledian distance
+def euclideanDistance(point1, point2):
+    return math.sqrt((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2)
+
+#Calculating distance
+distances = []
+for i in range(len(points)):
+    for j in range(i + 1, len(points)):
+        distance = euclideanDistance(points[i], points[j])
+        distances.append(distance)
+
+#Minimum distance
+min_distance = min(distances)
+print("Minimum distance:", min_distance)
